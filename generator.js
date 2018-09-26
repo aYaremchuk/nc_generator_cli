fb5d2ffd31fba46f11d0694be752271ffc9d6e4d
Andrew <jaremchuk.andrij@gmail.com>
2018-09-26 12:17:51 +0000

    Add minor fix change logic placement

--- generator.js
+++ generator.js
@@ -3,32 +3,30 @@ const lowercase = 'abcdefghijklmnopqrstuvwxyz';
 const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '0123456789';
 
-String.prototype.pick = function (min, max) {
-  let n; let
-    chars = '';
+function pick(value, min, max) {
+  let n;
+  let chars = '';
 
   if (typeof max === 'undefined') {
     n = min;
   } else {
     n = min + Math.floor(Math.random() * (max - min + 1));
   }
-
-  for (let i = 0; i < n; i++) {
-    chars += this.charAt(Math.floor(Math.random() * this.length));
+  for (let i = 0; i < n; i += 1) {
+    chars += value.charAt(Math.floor(Math.random() * value.length));
   }
 
   return chars;
-};
+}
 
-
-// Credit to @Christoph: http://stackoverflow.com/a/962890/464744
-String.prototype.shuffle = function () {
-  const array = this.split('');
-  let tmp; let current; let
-    top = array.length;
+function shuffle(value) {
+  const array = value.split('');
+  let tmp;
+  let current;
+  let top = array.length;
 
   if (top) {
-    while (--top) {
+    while (top -= 1) {
       current = Math.floor(Math.random() * (top + 1));
       tmp = array[current];
       array[current] = array[top];
@@ -37,17 +35,17 @@ String.prototype.shuffle = function () {
   }
 
   return array.join('');
-};
+}
 
 
 module.exports = {
   pswCreate: (minLength = 8, minUppercase = 1, minDigest = 1, minSpecial = 1) => {
     let password = '';
-    password += specials.pick(minSpecial);
-    password += uppercase.pick(minUppercase);
-    password += numbers.pick(minDigest);
-    password += lowercase.pick(minLength - password.length);
-    password = password.shuffle();
+    password += pick(specials, minSpecial);
+    password += pick(uppercase, minUppercase);
+    password += pick(numbers, minDigest);
+    password += pick(lowercase, minLength - password.length);
+    password = shuffle(password);
     return password;
   },
 };
--- passwordgen.js
+++ passwordgen.js
@@ -8,4 +8,14 @@ program
   .option('-s, --special <n>', 'count of special', parseInt)
   .parse(process.argv);
 
-generator.pswCreate(program.length, program.uppercase, program.digits, program.special);
+
+if (program.length < 8) {
+  console.log('less then minimal length');
+}
+
+const greaterLength = program.length - program.uppercase - program.digits;
+if (program.uppercase < greaterLength || program.digits < greaterLength || program.special < greaterLength) {
+  console.log('greater than possible');
+}
+
+console.log(generator.pswCreate(program.length, program.uppercase, program.digits, program.special));
